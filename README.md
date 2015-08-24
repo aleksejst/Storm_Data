4d05744ce21d5d94210f317593e20600582b5eea
aleksejst <aleksejst@users.noreply.github.com>
2015-08-24 22:37:33 +0100

    om ms

---
+++ README.md
@@ -0,0 +1,6 @@
+Storm Data - Coursera
+=========================================
+
+This is a temporary repostiory and will get romeved at some point.
+
+Included is another Coursera project - .RMD has all the details
---
+++ for storms data - names.R
@@ -0,0 +1,445 @@
+et1 <- "astronomical low tide"              # Astronomical Low Tide
+et2 <- "avalanche"                          # Avalanche 
+et3 <- c("blizzard",
+         "blizzard and extreme wind chil",
+         "blizzard/heavy snow",
+         "ground blizzard",
+         "heavy snow/blizzard",
+         "high wind/blizzard")              # Blizzard 
+et4 <- c("coastal flood",
+         "coastal flooding",
+         "coastal flooding/erosion",
+         "coastal/tidal flood",
+         "beach flood",
+         "erosion/cstl flood",
+         "tidal flooding",
+         "cstl flooding/erosion")           # Coastal flood
+et5 <- c("cold",
+         "cold temperature",
+         "cold wave",
+         "cold wind chill temperatures",
+         "high winds/cold",
+         "snow and cold",
+         "cold/wind chill",
+         "cold and frost",
+         "cold temperatures",
+         "cold weather",
+         "wind chill",
+         "snow/cold")                       # Cold/Wind Chill
+et6 <- NA                                   # Debris Flow
+et7 <- c("dense fog", 
+         "fog", 
+         "patchy dense fog")                # Dense Fog
+et8 <- c("dense smoke", 
+         "smoke")                           # Dense Smoke
+et9 <- c("drought",
+         "drought/excessive heat",
+         "snow drought")                    # Drought 
+et10 <- "dust devil"                        # Dust Devil
+et11 <- c("blowing dust", 
+          "dust storm")                     # Dust Storm
+et12 <- c("drought/excessive heat",
+          "record heat",
+          "excessive heat",
+          "record/excessive heat",
+          "extreme heat")                   # Excessive Heat
+et13 <- c("excessive cold", 
+          "extreme cold/wind chill",
+          "record cold",
+          "unseasonably cold",
+          "extreme wind chill",
+          "extreme windchill temperatures",
+          "bitter wind chill temperatures",
+          "cold/wind chill",
+          "extreme cold",
+          "extreme/record cold",
+          "record cold/frost",
+          "unusually cold",
+          "extreme cold/wind chill",
+          "extreme windchill",
+          "cold wind chill temperatures",
+          "wind chill")                     # Extreme Cold/Wind Chill
+et14 <- c("flash flood",
+          "flash flood/ flood",
+          "flash flooding/flood",
+          "flood/flash",
+          "flash flood - heavy rain",
+          "flash flood/flood",
+          "flash floods",
+          "flood/flash flood",
+          "flash flood from ice jams",
+          "flash flooding",
+          "flood flash",
+          "flood/flash flooding")           # Flash Flood
+et15 <- c("urban/street flooding",
+          "urban/small stream flooding",
+          "urban/small stream flood",
+          "urban/small stream  flood",
+          "urban small stream flood",
+          "urban floods",
+          "urban flooding",
+          "urban flood",
+          "urban and small stream floodin",
+          "urban and small stream flood",
+          "thunderstorm winds/ flood",
+          "street flooding",
+          "street flood",
+          "snowmelt flooding",
+          "small stream/urban flood",
+          "small stream flooding",
+          "small stream flood",
+          "small stream and urban flood",
+          "rural flood",
+          "river flooding",
+          "river flood",
+          "river and stream flood",
+          "minor flooding",
+          "major flood",
+          "ice jam flooding",
+          "heavy rains/flooding",
+          "heavy rain/flooding",
+          "floods",
+          "flooding",
+          "flood/flash flooding",
+          "flood",
+          "flood & heavy rain")             # Flood 
+et16 <- c("frost",
+          "frost/freeze",
+          "snow/sleet/freezing rain",
+          "snow/freezing rain",
+          "snow freezing rain",
+          "sleet/freezing rain",
+          "light freezing rain",
+          "heavy snow/freezing",
+          "hard freeze",
+          "frost/freeze",
+          "freezing rain/snow",
+          "freezing rain/sleet",
+          "freezing rain",
+          "freezing rain and sleet",
+          "freezing drizzle",
+          "freeze",
+          "damaging freeze",
+          "agricultural freeze")            # Frost/Freeze
+et17 <- c("cold air funnel",
+          "funnel",
+          "funnel clouds",
+          "cold air funnels",
+          "funnel cloud",
+          "thunderstorm winds funnel clou") # Funnel Cloud
+et18 <- c("freezing fog",
+          "ice fog")                        # Freezing Fog
+et19 <- c("hail",
+          "hail 1.75",
+          "hail 175",
+          "hail 80",
+          "hail/winds",
+          "non severe hail",
+          "thunderstorm winds/hail",
+          "hail 0.75",
+          "hail 100",
+          "hail 275",
+          "hail damage",
+          "hailstorm",
+          "small hail",
+          "tstm wind/hail",
+          "hail 1.00",
+          "hail 150",
+          "hail 75",
+          "hail/wind",
+          "marine hail",
+          "thunderstorm winds hail")        # Hail
+et20 <- c("heat",
+          "heat wave",
+          "heat waves")                     # Heat
+et21 <- c("excessive rain",
+          "unseasonal rain",
+          "tstm heavy rain",
+          "snow/sleet/freezing rain",
+          "snow/freezing rain",
+          "snow freezing rain",
+          "sleet/freezing rain",
+          "record rainfall",
+          "record low rainfall",
+          "rain/snow",
+          "rain",
+          "prolonged rain",
+          "monthly rainfall",
+          "light freezing rain",
+          "hvy rain",
+          "heavy rains/flooding",
+          "heavy rains",
+          "heavy rainfall",
+          "heavy rain/wind",
+          "heavy rain/severe weather",
+          "heavy rain/flooding",
+          "heavy rain",
+          "heavy rain and wind",
+          "flood & heavy rain",
+          "flash flood - heavy rain",
+          "excessive rainfall")             # Heavy Rain
+et22 <- c("snow squalls",
+          "snow squall",
+          "snow and heavy snow",
+          "record winter snow",
+          "record snowfall",
+          "record snow",
+          "high wind/heavy snow",
+          "heavy snow-squalls",
+          "heavy snow/squalls",
+          "heavy snow/ice",
+          "heavy snow/ice storm",
+          "heavy snow/freezing rain",
+          "heavy snow/blizzard",
+          "heavy snow",
+          "heavy snow squalls",
+          "heavy snow and ice",
+          "heavy snow and ice storm",
+          "heavy snow and high winds",
+          "heavy lake snow",
+          "excessive snow",
+          "blizzard/heavy snow",
+          "accumulated snowfall")           # Heavy Snow
+et23 <- c( "heavy surf",
+           "heavy surf/high surf",
+           "high surf", 
+           "high surf advisory", 
+           "rip currents/heavy surf", 
+           "rough surf")                    # High Surf
+et24 <- c("gusty thunderstorm winds",
+          "gusty winds",
+          "heavy rain/wind",
+          "high wind",
+          "high wind and high tides",
+          "high wind/blizzard",
+          "high winds",
+          "high winds 66",
+          "high winds/cold",
+          "severe thunderstorm winds",
+          "strong wind",
+          "strong winds",
+          "whirlwind",
+          "gusty wind",
+          "hail/wind",
+          "heavy snow and high winds",
+          "high wind (g40)",
+          "high wind damage",
+          "high wind/heavy snow",
+          "high winds 63",
+          "high winds 80",
+          "high winds/snow",
+          "snow/high winds",
+          "strong wind gust",
+          "thuderstorm winds",
+          "wind",
+          "wind gusts")                     # High Wind
+et25 <- c("hurricane",
+          "hurricane erin",
+          "hurricane/typhoon",
+          "hurricane-generated swells",
+          "hurricane felix",
+          "hurricane edouard",
+          "hurricane opal",
+          "typhoon")                        # Hurricane (Typhoon)
+et26 <- c("falling snow/ice", 
+          "heavy snow and ice",
+          "heavy snow and ice storm",
+          "heavy snow/ice",
+          "heavy snow/ice storm",
+          "ice storm",
+          "snow/ice storm")                 # Ice Storm
+et27 <- c("heavy lake snow",
+          "lake-effect snow",
+          "lake effect snow" )              # Lake-Effect Snow
+et28 <- "lakeshore flood"                   # Lakeshore Flood
+et29 <- c( "lightning",
+           "thunderstorm winds lightning")  # Lightning
+et30 <- "marine hail"                       # Marine Hail
+et31 <- "marine high wind"                  # Marine High Wind
+et32 <- "marine strong wind"                # Marine Strong Wind
+et33 <- c("marine thunderstorm wind",
+          "marine tstm wind")               # Marine Thunderstorm Wind
+et34 <- c("rip current",
+          "rip currents",
+          "rip currents/heavy surf")        # Rip Current
+et35 <- "seiche"                            # Seiche
+et36 <- c("coastal storm",
+          "storm surge",
+          "storm surge/tide",
+          "coastal surge")                  # Storm Surge/Tide
+et37 <- c( "strong wind",
+           "strong wind gust",
+           "strong winds",
+           "gusty wind",
+           "gusty winds",
+           "non tstm wind",
+           "wind",
+           "wind advisory",
+           "wind damage",
+           "wind gusts",
+           "winds")                         # Strong Wind
+et38 <- c("gusty thunderstorm wind",
+          "tstm winds",
+          "tstm wind/hail",
+          "tstm wind",
+          "tstm wind 55",
+          "tstm wind 52",
+          "tstm wind 51",
+          "tstm wind (g45)",
+          "tstm wind (g40)",
+          "tstm heavy rain",
+          "thundertorm winds",
+          "thunderstrom winds",
+          "thunderstormw winds",
+          "thunderstorms",
+          "thunderstorms winds",
+          "thunderstorms wind",
+          "thunderstorm",
+          "thunderstorm windss",
+          "thunderstorm winds/hail",
+          "thunderstorm winds/ flood",
+          "thunderstorm winds.",
+          "thunderstorm winds",
+          "thunderstorm winds lightning",
+          "thunderstorm winds hail",
+          "thunderstorm winds g",
+          "thunderstorm winds funnel clou",
+          "thunderstorm winds and",
+          "thunderstorm wind/ trees",
+          "thunderstorm wind",
+          "thunderstorm wind g60",
+          "thunderstorm wind g52",
+          "thunderstorm wind g50",
+          "thunderstorm wind 60 mph",
+          "thunderstorm wind 50",
+          "thunderstorm damage",
+          "thunderstorm  winds",
+          "severe thunderstorms",
+          "severe thunderstorm",
+          "severe thunderstorm winds",
+          "marine tstm wind",
+          "marine thunderstorm wind",
+          "gusty thunderstorm winds",
+          " tstm wind")                     # Thunderstorm Wind
+et39 <- c("tornado",
+          "waterspout-tornado",
+          "waterspout/tornado",
+          "waterspout/ tornado",
+          "tornadoes",
+          "tornado f3",
+          "tornado f2",
+          "tornado f1",
+          "tornado f0")                     # Tornado
+et40 <- "tropical depression"               # Tropical Depression
+et41 <- c("tropical depression",
+          "tropical storm",
+          "tropical storm dean",
+          "tropical storm jerry")           # Tropical Storm
+et42 <- "tsunami"
+et43 <- c("volcanic ash",
+          "volcanic ashfall")               # Volcanic Ash
+et44 <- c("waterspout",
+          "waterspout-",
+          "waterspout-tornado",
+          "waterspout/ tornado",
+          "waterspout/tornado",
+          "waterspouts")                    # Waterspout 
+et45 <- c("wild fires",
+          "wild/forest fire",
+          "wildfire",
+          "wildfires",
+          "brush fire")                     # Wildfire
+et46 <- c("winter storm",
+          "winter storms")                  # Winter Storm
+et47 <- c("winter mix",
+          "winter weather",
+          "winter weather mix",
+          "winter weather/mix",
+          "wintery mix",
+          "wintry mix")                     # Winter Weather
+et48 <- c("freezing rain and sleet",
+          "freezing rain/sleet",
+          "light snow and sleet",
+          "sleet",
+          "sleet storm",
+          "sleet/freezing rain",
+          "sleet/snow",
+          "snow and sleet",
+          "snow/sleet",
+          "snow/sleet/freezing rain")       # Sleet
+
+EvClass <- list(et1, et2, et3, et4, et5, et6, et7, et8, et9, et10, et11, et12, 
+             et13, et14, et15, et16, et17, et18, et19, et20, et21, et22, et23, 
+             et24, et25, et26, et27, et28, et29, et30, et31, et32, et33, et34, 
+             et35, et36, et37, et38, et39, et40, et41, et42, et43, et44, et45,
+             et46, et47, et48)
+EvName <- c("Astronomical Low Tide", "Avalanche", "Blizzard", "Coastal Flood",
+            "Cold/Wind Chill", "Debris Flow", "Dense Fog", "Dense Smoke",
+            "Drought", "Dust Devil", "Dust Storm", "Excessive Heat", 
+            "Extreme Cold/Wind Chill", "Flash Flood", "Flood", "Frost/Freeze",
+            "Funnel Cloud", "Freezing Fog", "Hail", "Heat", "Heavy Rain",
+            "Heavy Snow", "High Surf", "High Wind", "Hurricane (Typhoon)",
+            "Ice Storm", "Lake-Effect Snow", "Lakeshore Flood", "Lightning",
+            "Marine Hail", "Marine High Wind", "Marine Strong Wind",
+            "Marine Thunderstorm Wind", "Rip Current", "Seiche", "Sleet",
+            "Storm Surge/Tide", "Strong Wind", "Thunderstorm Wind", "Tornado",
+            "Tropical Depression", "Tropical Storm", "Tsunami", "Volcanic Ash",
+            "Waterspout", "Wildfire", "Winter Storm", "Winter Weather", "Sleet")
+
+
+for (i in 1:length(EvName)){
+    freqEvS[freqEvS$EvID %in% EvClass[[i]], 3+i] <- EvName[i]
+}
+names(freqEvS) <- c("Freq", "EvID", "Pct",  EvName)
+
+
+
+
+
+
+
+subset(freqEvS, EvID %in% et4)
+
+
+
+sapply(et3, grepl, freqEvS$EvID)
+freqEvS[sapply(et3, grepl, freqEvS$EvID),5]
+freqEvS[ grep("astronomical low tide", 
+              freqEvS$EvID), 4] <- "Astronomical Low Tide"
+
+
+
+
+
+
+Class1 <- "aa"                  # A Only
+Class2 <- "bb"                  # B Only
+Class3 <- c("ab", "ba")         # Diff symbols
+Class4 <- c("ab", "ba", "aa")   # All A
+Class5 <- c("ab", "ba", "bb")   # All B
+
+Classes <- list(Class1, Class2, Class3, Class4, Class5)
+ClassName <- c("A Only", "B Only", "Diff symbols", "All A", "All B")
+
+
+for (i in 1:length(ClassName)){
+    df[df$col2 %in% Classes[[i]], 2+i] <- "x"
+}
+names(df)[3:7] <- ClassName
+
+tcdf <- data.frame()
+
+for (i in 1:length(EvName)){
+    if (i != 6) {
+        tdf <- aggregate(cbind(fatalities, injuries) ~ mydataC[, i + 10], sum, data = mydataC)
+        names(tdf) <- c("evtype", "fatalities", "injuries")
+        tdf$evcount <- length(subset(mydataC[, i + 10], 
+                                     mydataC [, i + 10] != "NA"))
+        tcdf <- rbind(tcdf, tdf)
+    }
+}
+
+
+
+
+
